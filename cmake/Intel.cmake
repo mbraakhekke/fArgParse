2d3530bdad3cf349966758e320ad18ef254369fd
Maarten Braakhekke <maarten.braakhekke@wur.nl>
2021-04-28 15:24:50 +0200

    Modifications to Intel compiler flags when compiling on Windows

--- cmake/Intel.cmake
+++ cmake/Intel.cmake
@@ -3,20 +3,26 @@
 if(WIN32)
   set(no_optimize "-Od")
   set(check_all "-check:all")
+  set(save-temps "-Qsave-temps")
+  set(disable_warning_for_long_names "-Qdiag-disable:5462")
+  set(cpp "-fpp")
+  set(debug_info "-Zi")
 else()
   set(no_optimize "-O0")
   set(check_all "-check all")
+  set(save-temps "-save-temps")
+  set(disable_warning_for_long_names "-diag-disable 5462")
+  set(cpp "-cpp")
+  set(debug_info "-g")
 endif()
   
 
-set(disable_warning_for_long_names "-diag-disable 5462")
 set(traceback "-traceback")
-set(cpp "-cpp")
 
 
 set(CMAKE_Fortran_FLAGS_DEBUG  "${no_optimize}")
 set(CMAKE_Fortran_FLAGS_RELEASE "-O3")
-set(CMAKE_Fortran_FLAGS "-g ${cpp} ${traceback} ${check_all} ${disable_warning_for_long_names} -save-temps")
+set(CMAKE_Fortran_FLAGS "${debug_info} ${cpp} ${traceback} ${check_all} ${disable_warning_for_long_names} ${save-temps}")
 
 add_definitions(-D_INTEL)
 add_definitions(-D__ifort_18)
